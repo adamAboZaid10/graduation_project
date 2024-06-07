865d638828d50d1d5a2f1fc6cc334f7bb612fab0
PC <khaldmhmwdalmdhyn@gmail.com>
2024-06-07 22:47:56 +0300

    illness image data

--- lib/features/home_features/screen/home_page.dart
+++ lib/features/home_features/screen/home_page.dart
@@ -17,10 +17,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List images = [
-    "assets/image/onboarding1.jpg",
+    "assets/image/ckd_image.webp",
     "assets/image/onboarding2.jpg",
-    "assets/image/onboarding3.jpg",
-    "assets/image/onboarding4.jpg"
+    "assets/image/liver.jpeg",
+    // "assets/image/onboarding4.jpg"
   ];
 
   @override
--- lib/features/home_features/widgets/ckd_widgets/illness_screen_body.dart
+++ lib/features/home_features/widgets/ckd_widgets/illness_screen_body.dart
@@ -16,21 +16,23 @@ class CKDBody extends StatefulWidget {
 }
 
 class _CKDBodyState extends State<CKDBody> {
-  String token = '';
-
+  String? token;
   @override
   void initState() {
     super.initState();
-    loadData();
+    setState(() {
+      loadData();
+    });
   }
 
   void loadData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    token = prefs.getString("token") ?? "";
-    setState(() {});
+    setState(() {
+      token = prefs.getString("token") ?? "";
+    });
+    print("@@@@@@@@@@@@@@@@@@@ $token");
   }
 
-  @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeControllerCubit, HomeControllerState>(
         builder: (context, state) {
@@ -148,10 +150,13 @@ class _CKDBodyState extends State<CKDBody> {
                     )
                   : CustomDefaultButton(
                       onPressed: () {
-                        cubit.postIllnessData(
-                          imagePath: cubit.selectedImage!,
-                          token: token,
-                        );
+                        setState(() {
+                          cubit.postIllnessData(
+                            imagePath: cubit.selectedImage!,
+                            token: token,
+                          );
+                        });
+                        print("############# $token #################");
                       },
                       color: AppColor.blueWhiteColor,
                       text: 'Show Result',
